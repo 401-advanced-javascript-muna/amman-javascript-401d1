fb254cd15ecaf92adc1921e914ec1c9ad1b101e0
Muna <55940768+munaalshorman@users.noreply.github.com>
2020-01-25 16:26:58 +0200

    Update reader.js

--- class-03/demo/files/lib/reader.js
+++ class-03/demo/files/lib/reader.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs'); //file system (utility meethod to do things) 
 const util = require('util');
 
 // Wrap the fs.readFile method with our interface so that we can properly test it modularly.
 const readerWithCallback = (file, callback) => {
-  fs.readFile(file, (err, data) => {
-    if (err) { callback(err); }
-    else { callback(undefined, data.toString().trim()); }
+  fs.readFile(file, (err, data) => {    // readFile is a method  fs.readFile(file, (err, data):callback if my file did not work readFile will
+//pass something to err(if =not null that is mean the reader missed up)
+    if (err) { callback(err); }  // to handle my error
+   callbackn(null, data.toString().trim());  // null that is mean data is ok ,,when I read from disk the data will be binary (buffer)
   });
 };
 
