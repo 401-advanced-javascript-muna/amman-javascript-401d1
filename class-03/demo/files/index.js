2049ca69cb5b2242c9d3f3766e18809e3c914e0f
Muna <55940768+munaalshorman@users.noreply.github.com>
2020-01-25 17:30:17 +0200

    Update index.js

--- class-03/demo/files/index.js
+++ class-03/demo/files/index.js
@@ -12,13 +12,18 @@ let file = `${__dirname}/data/file.txt`; // absolute bath
 // Callback Style
 fs.readFile(file, (err, data) => {
   if (err) { throw err; }
-  console.log('Callback', data.toString().trim());// data is buffer .trim() to remove space
+  console.log('Callback', data.toString().trim());// data is a content of the and presented as buffer .trim() to remove space
 });
 
 // Promise Style
 // First, turn the normal fs.readFile into a promise
 let readFilePromise = util.promisify(fs.readFile) //to use utility function :promsify function ,promsify s.t to use it
-// take the contint of (fs.readFile) 
+// take the contint of (fs.readFile)
+
+// util.promisifyconverts a regular function into an async function, i.e. a function that returns a promise
+// The function passed to util.promisify must follow the NodeJS callback style. The function must pass a callback as the last parameter, 
+// and the callback must be the take the following parameters in the following order: (err, value) => { /* … */ }
+// Promisified functions can be used with await and async to help avoid messy promise chains and introduce a cleaner, saner, way to do asynchronous programming.
 
 // then, call it like any other promise
 readFilePromise(file)
@@ -50,17 +55,18 @@ reader.readerWithCallback(file, (err, data) => {
 // then, call it like any other promise
 reader.readerWithPromise(file)
   .then(data => console.log('Promise from module', data))
-  .catch(err => { throw err; });
+  .catch(err => { // console.error('my error',err)  //get back abuffer
+  throw err; });
 
 
 // Async/Await Style
 // Need to make an async function and invoke it
-async function readFileAsyncFromModule(file) {
-  try {
-    let data = await reader.readerWithPromise(file);
+async function readFileAsyncFromModule(file) {  // async reseved keyword which meaning this acync fun so it is need to await(reseved keyword)
+  try {                                         // try and catch
+    let data = await reader.readerWithPromise(file); // when data is avilable let me use it
     console.log('Async from module', data);
   }
   catch (err) { throw err }
 }
 
-readFileAsyncFromModule(file);
+readFileAsyncFromModule(file); // call a function
